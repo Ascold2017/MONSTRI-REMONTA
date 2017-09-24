fecc265a433aff3d709c6229423a8d81fe06b11e
Ascold2017 <ascold96@gmail.com>
2017-09-24 15:06:53 +0300

    header apadtive home fix 1

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,27 @@
-;"use strict";
-$(document).ready(function(){
-   // Пользовательские функции 
-
+;
+"use strict";
+$(document).ready(function () {
+    // Пользовательские функции 
+    $(".mobile_menu__link.toggle").on('click', function (e) {
+        e.preventDefault();
+        $(".mobile_menu_list").slideToggle();
+    });
+    $(".home_slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: false,
+                },
+            },
+        ],
+    })
 });
